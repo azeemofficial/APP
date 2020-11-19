861fe046196d4ecdcef25994af4d54287e19a65f
azeemofficial <47776433+azeemofficial@users.noreply.github.com>
2020-11-19 14:10:25 +0500

    Create profitfinder.py

---
+++ profitfinder.py
@@ -0,0 +1,33 @@
+'''
+
+                            Online Python Interpreter.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+Let say inv amount is 100
+program will add 10 each time
+'''
+   
+def percentcalc(inv):
+    thismonth=inv*0.08
+    print("This month profit: {}".format(thismonth))
+    return thismonth
+
+def monthlyprofbonus(prc):
+    proftbon=prc*0.07
+    print("Profit bonus: {}".format(proftbon))
+    return proftbon
+    
+def addamount(inv):
+    perc=percentcalc(inv)
+    mpb=monthlyprofbonus(perc)
+    addedamount=inv+perc+mpb+50000
+    return addedamount
+ 
+print("Hello World")
+inv=int(input("Enter amount: "))
+bonus=inv*0.1
+inv=inv+bonus
+i=int(input("Enter amount of months: "))
+for a in range(i):
+    print("\n \nMonth {} : {}".format(a+1,inv))
+    inv=addamount(inv)
